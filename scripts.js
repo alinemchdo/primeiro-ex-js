a7370d985ee18fb77ef904dde1b3b849c7e3ae75
Aline Machado <alinedasmachado@gmail.com>
2023-02-24 14:02:36 -0300

    exercicos assincrono

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let trafficLight = 'roxo';
+
+switch(trafficLight){
+    case 'vermelho':
+        console.log('pare')
+        break;
+
+        case 'amarelo':
+        console.log('atenção')
+        break;
+
+        case 'verde':
+        console.log('vaiqvai')
+        break;
+
+        default:
+            console.log('valor n identificado');
+}
