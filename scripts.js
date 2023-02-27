d48f4c697bfdc126ae871053d948901590e6dec1
Aline Machado <alinedasmachado@gmail.com>
2023-02-27 17:33:43 -0300

    Aula gus js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,14 @@
-let trafficLight = 'roxo';
+let num1 = 80;
+let num2 = 83;
 
-switch(trafficLight){
-    case 'vermelho':
-        console.log('pare')
-        break;
 
-        case 'amarelo':
-        console.log('atenção')
-        break;
-
-        case 'verde':
-        console.log('vaiqvai')
-        break;
-
-        default:
-            console.log('valor n identificado');
+if(num1 > num2){
+    console.log('num1 wins')
+    
+} else if(num1 == num2){
+    console.log('Empate!')
+} else{
+    console.log('num2 wins')
 }
+
+ console.log(1 + '1');
